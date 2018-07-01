c6aa743c3834bbf8c340430ef5b6c62e56b5cc3a
rawenfalls <zaqmond@yandex.ru>
2018-07-01 13:13:49 +0300

    v0.1

--- rpn2.0.rb
+++ rpn2.0.rb
@@ -1,6 +1,7 @@
 expression = gets.chomp.strip.scan(/\S/)
 output=[]
 operands=[]
+priority = {"+" => 1, "-" => 1, "*" => 2, "/" => 2, "^" => 3}
 
 
 expression.each do |the_element_of_expression|
@@ -8,14 +9,18 @@ expression.each do |the_element_of_expression|
     when /[\+\-\*\/\^\(\)]+/
       operands<<the_element_of_expression
       if operands[1]!=nil
-        if operands[-1] == operands[-2] and operands[-1] != "(" and operands[-1] != ")"
-          output<<operands.delete_at(-2)
+        if operands[-1] != "(" and operands[-1] != ")" and operands[-2] != "("
+          if priority[operands[-1]] <= priority[operands[-2]]
+            output<<operands.delete_at(-2)
+          end
         end
         if operands[-1] == ")"
-          operands.delete_at(-1)
-          output<<operands.delete_at(-1)
-          operands.delete_at(-1)
-          output<<operands.delete_at(-1)
+          operands.pop
+          loop do
+            break if operands[-1]=="("
+            output<<operands.pop
+          end
+          operands.pop
         end
       end
     else
@@ -24,5 +29,8 @@ expression.each do |the_element_of_expression|
 end
 operands.delete("(")
 operands.delete(")")
-output<<operands
+loop do
+  break if operands[0]==nil
+  output<<operands.pop
+end
 puts output.join("")
